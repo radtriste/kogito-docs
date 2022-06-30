fc2f56aca9916692e300d62063aaf38ab3b88a1e
radtriste <tradisso@redhat.com>
2022-06-30 14:57:19 +0200

    Setup quarkus update job

--- .ci/jenkins/dsl/test.sh
+++ .ci/jenkins/dsl/test.sh
@@ -2,9 +2,10 @@
 
 script_dir_path=$(cd `dirname "${BASH_SOURCE[0]}"`; pwd -P)
 
-export DSL_DEFAULT_MAIN_CONFIG_FILE_REPO=kiegroup/kogito-pipelines
-export DSL_DEFAULT_MAIN_CONFIG_FILE_REF=main
+export DSL_DEFAULT_MAIN_CONFIG_FILE_REPO=radtriste/kogito-pipelines
+export DSL_DEFAULT_MAIN_CONFIG_FILE_REF=docs_quarkus_update_test
 export DSL_DEFAULT_MAIN_CONFIG_FILE_PATH=dsl/config/main.yaml
+export DSL_DEFAULT_BRANCH_CONFIG_BRANCH=docs_quarkus_update_test
 
 file=$(mktemp)
 # For more usage of the script, use ./test.sh -h
